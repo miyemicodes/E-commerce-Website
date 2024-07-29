9f56f281a7128672b0dbcb521f57af2ca6804ccc
miyemicodes <lizzymiyemi@gmail.com>
2024-07-29 01:01:03 -0700

    E-commerce-Website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import react from "react";
 
 function App() {
-  return <>
-    <h1>Hi LiLi</h1>
-  </>;
+  return (
+    <>
+      <h1>Hi LiLi E-commerce-Website</h1>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <section>
+        <div className="flex flex-col">
+          <div>
+            <h3>Where about</h3>
+            <p>7 Washington Ave.Kentucky 38950</p>
+          </div>
+          <div>
+            <h3>Contact</h3>
+            <p>(808)389-5301</p>
+          </div>
+          <div>
+            <h3>Where about</h3>
+            <p>lizzymiyemi@gmail.com</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </section>
+    </>
+  );
+}
