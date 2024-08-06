44886ceed835fef3083546f60628c9c6ead22192
miyemicodes <lizzymiyemi@gmail.com>
2024-08-06 09:31:10 -0700

    E-commerce

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 // import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutUs from "./Components/AboutUs";
 import Home from "./Components/Home";
 import ProductDetails from "./Components/ProductDetail";
@@ -6,16 +7,25 @@ import ProductDetails from "./Components/ProductDetail";
 function App() {
   return (
     <>
-      <Home />
-      <ProductDetails />
-      <AboutUs/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/productdetail" Component={ProductDetails} />
+          <Route path="/about" Component={AboutUs} />
+        </Routes>
+      </BrowserRouter>
 
-      {/* <Router>
+      {/*
+      <Router>
         <Routes>
           <Route exact path="/" Component={<Home />} />
           <Route path="/productDetails" Component={<ProductDetails />} />
         </Routes>
-      </Router> */}
+      </Router>
+       <Home />
+      <ProductDetails />
+      <AboutUs /> 
+      */}
     </>
   );
 }
--- src/Components/ShopNow.jsx
+++ src/Components/ShopNow.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { FaCartArrowDown } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+import ProductDetails from "./ProductDetail";
 
 export default function Button() {
   return (
     <>
-
-      <button className="bg-[#967f50] text-sm flex items-center justify-center gap-2 px-6 py-2 text-[#fbf3e2] border border-[#fbf3e2] rounded-sm w-36">
-        <p>Shop Now</p>
-        <FaCartArrowDown />
-      </button>
+      <Link to={ProductDetails}>
+        <button className="bg-[#967f50] text-sm flex items-center justify-center gap-2 px-6 py-2 text-[#fbf3e2] border border-[#fbf3e2] rounded-sm w-36">
+          <p>Shop Now</p>
+          <FaCartArrowDown />
+        </button>
+      </Link>
     </>
   );
 }
