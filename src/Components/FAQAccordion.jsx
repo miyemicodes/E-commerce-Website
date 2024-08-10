cd9628501f89f63cbbadc27c4f20a0690263e7c1
miyemicodes <lizzymiyemi@gmail.com>
2024-08-10 00:55:23 -0700

    E-commerce

--- src/Components/FAQAccordion.jsx
+++ src/Components/FAQAccordion.jsx
@@ -4,11 +4,21 @@ import faqs from "./FaqFile";
 
 const FAQAccordion = () => {
   return (
-    <div className="max-w-xl mx-auto mt-10">
-      {faqs.map((faq, index) => (
-        <FaqItem key={index} question={faq.question} answer={faq.answer} />
-      ))}
-    </div>
+    <section className="bg-[#ededea]  py-16">
+      <div className="flex flex-col items-center justify-center gap-3">
+        <span className="text-sm"> Ask Us Anything</span>
+        <h1 className="text-4xl font-serif">Frequently Asked Questions</h1>
+        <p className="text-sm">
+          Our friendly team would love to answer your questions.
+        </p>
+      </div>
+
+      <div className="max-w-xl mx-auto mt-10">
+        {faqs.map((faq, index) => (
+          <FaqItem key={index} question={faq.question} answer={faq.answer} />
+        ))}
+      </div>
+    </section>
   );
 };
 
--- src/Components/FaqItem.jsx
+++ src/Components/FaqItem.jsx
@@ -4,17 +4,24 @@ const FaqItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="border-b">
+    <div
+      className="border-b-4 rounded-md px-3 text-gray-700"
+      style={
+        isOpen
+          ? { color: "#f2ece3", background: "#977f4a" }
+          : { color: "black", background: "white" }
+      }
+    >
       <button
         onClick={() => setIsOpen(!isOpen)}
         className="w-full text-left py-4 flex justify-between items-center focus:outline-none"
       >
-        <span>{question}</span>
-        <span>{isOpen ? "-" : "+"}</span>
+        <span className="font-semibold"> {question}</span>
+        <span className="border px-1">{isOpen ? "-" : "+"}</span>
       </button>
       {isOpen && (
         <div className="pb-4">
-          <p className="text-gray-700">{answer}</p>
+          <p className="text-sm">{answer}</p>
         </div>
       )}
     </div>
