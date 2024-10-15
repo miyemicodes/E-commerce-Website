8254e7fdb46ebc3d5a411d564680f2d7d777ca86
miyemicodes <lizzymiemi@gmail.com>
2024-10-15 08:16:03 +0100

    Updates

--- src/Components/CoverTop.jsx
+++ src/Components/CoverTop.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 import "../index.css";
+import { useLocation } from "react-router-dom";
 
 export default function Cover() {
-  return (
-    <>
-      <section className="cover flex items-center justify-center">
-        <h2 className="flex items-center justify-center text-3xl text-white font-semibold font-serif">
-          Home/Shop
-        </h2>
-      </section>
-    </>
-  );
+	const location = useLocation();
+
+	const getPageName = () => {
+		switch (location.pathname) {
+			case "/shop":
+				return "Browse Our Products";
+			case "/about":
+				return "About Our Shop";
+			case "/productdetail":
+				return "Product Details";
+			default:
+				return "E-commerce Site";
+		}
+	};
+
+	return (
+		<>
+			<section className="cover flex items-center justify-center">
+				<h2 className="flex items-center justify-center text-4xl text-black font-semibold font-serif">
+					{getPageName()}
+				</h2>
+			</section>
+		</>
+	);
 }
